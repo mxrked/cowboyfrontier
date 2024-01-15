2cdf2d67dbe1da6505484eebb72220a91f705480
Parker <pp101socials@gmail.com>
2024-01-15 11:41:57 -0500

    Added cdns

--- assets/cdns/CDNBgs.js
+++ assets/cdns/CDNBgs.js
@@ -6,9 +6,41 @@
 
 import { CDNBGReturn } from "./CDNReturns";
 
+// Index
 const INDEX_TOP_BG = CDNBGReturn("index", "top-bg", "webp");
-const INDEX_STORE_BG = CDNBGReturn("index", "store-bg", "webp");
-const INDEX_INFO_BG = CDNBGReturn("index", "info-bg", "webp");
-const INDEX_CONTACT_BG = CDNBGReturn("index", "contact-bg", "webp");
+const INDEX_STORE_BG_1 = CDNBGReturn("index", "store-bg-1", "webp");
+const INDEX_STORE_BG_2 = CDNBGReturn("index", "store-bg-2", "webp");
+const INDEX_INFO_BG_1 = CDNBGReturn("index", "info-bg-1", "webp");
+const INDEX_INFO_BG_2 = CDNBGReturn("index", "info-bg-2", "webp");
+const INDEX_CONTACT_BG_1 = CDNBGReturn("index", "contact-bg-1", "webp");
+const INDEX_CONTACT_BG_2 = CDNBGReturn("index", "contact-bg-2", "webp");
+const INDEX_HATS_BG = CDNBGReturn("index", "hats-bg", "webp");
+const INDEX_BOOTS_BG = CDNBGReturn("index", "boots-bg", "webp");
+const INDEX_SADDLES_BG = CDNBGReturn("index", "saddles-bg", "webp");
+const INDEX_GUNS_BG = CDNBGReturn("index", "guns-bg", "webp");
 
-export { INDEX_TOP_BG, INDEX_STORE_BG, INDEX_INFO_BG, INDEX_CONTACT_BG };
+// Info
+const INFO_TOP_BG = CDNBGReturn("info", "top-bg", "webp");
+
+// Store
+const STORE_TOP_BG = CDNBGReturn("store", "top-bg", "webp");
+
+// Contact
+const CONTACT_TOP_BG = CDNBGReturn("contact", "top-bg", "webp");
+
+export {
+  INDEX_BOOTS_BG,
+  INDEX_CONTACT_BG_1,
+  INDEX_CONTACT_BG_2,
+  INDEX_GUNS_BG,
+  INDEX_HATS_BG,
+  INDEX_INFO_BG_1,
+  INDEX_INFO_BG_2,
+  INDEX_SADDLES_BG,
+  INDEX_STORE_BG_1,
+  INDEX_STORE_BG_2,
+  INDEX_TOP_BG,
+};
+export { INFO_TOP_BG };
+export { STORE_TOP_BG };
+export { CONTACT_TOP_BG };
